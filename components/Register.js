56b15dbad50b422a15b6ce416afdcfabbcd32ee5
Jason Davidson <jasonftw625@gmail.com>
2022-09-22 11:19:11 +0200

    Fix some stuff

--- components/Register.js
+++ components/Register.js
@@ -49,32 +49,32 @@ export default function Register() {
             <small>Enter your credentials to create your account</small>
           </div>
 
-          <div class="relative border border-gray-400 rounded-lg mt-10">
+          <div className="relative border border-gray-400 rounded-lg mt-10">
             <input
               id="name"
               name="name"
               type="email"
-              class="w-full  text-white placeholder-transparent  bg-white bg-opacity-10  peer focus:outline-none "
+              className="w-full  text-white placeholder-transparent  bg-white bg-opacity-10  peer focus:outline-none "
               placeholder="Name"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               autoComplete="off"
             />
             <label
-              for="name"
-              class="absolute left-0 pl-2 -top-7 text-white font-bold text-md transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-white peer-placeholder-shown:top-2 peer-focus:-top-7 peer-focus:text-white peer-focus:text-md"
+              form="name"
+              className="absolute left-0 pl-2 -top-7 text-white font-bold text-md transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-white peer-placeholder-shown:top-2 peer-focus:-top-7 peer-focus:text-white peer-focus:text-md"
             >
               Email:
             </label>
           </div>
 
-          <div class="relative border border-gray-400 rounded-lg bg-white bg-opacity-10 mt-10 flex">
+          <div className="relative border border-gray-400 rounded-lg bg-white bg-opacity-10 mt-10 flex">
             <div>
               <input
                 id="name"
                 name="name"
                 type={password1.show ? "text" : "password"}
-                class="w-full  text-white placeholder-transparent  bg-white bg-opacity-0  peer focus:outline-none "
+                className="w-full  text-white placeholder-transparent  bg-white bg-opacity-0  peer focus:outline-none "
                 placeholder="Name"
                 value={password1.value}
                 onChange={(e) => {
@@ -86,8 +86,8 @@ export default function Register() {
                 autoComplete="off"
               />
               <label
-                for="name"
-                class="absolute left-0 pl-2 -top-7 text-white font-bold text-md transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-white peer-placeholder-shown:top-2 peer-focus:-top-7 peer-focus:text-white peer-focus:text-md"
+                form="name"
+                className="absolute left-0 pl-2 -top-7 text-white font-bold text-md transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-white peer-placeholder-shown:top-2 peer-focus:-top-7 peer-focus:text-white peer-focus:text-md"
               >
                 Password:
               </label>
@@ -106,18 +106,18 @@ export default function Register() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="w-6 h-6"
+                  className="w-6 h-6"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
                   />
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                   />
                 </svg>
@@ -126,26 +126,26 @@ export default function Register() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="w-6 h-6"
+                  className="w-6 h-6"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
                   />
                 </svg>
               )}
             </div>
           </div>
-          <div class="relative border border-gray-400 rounded-lg bg-white bg-opacity-10 mt-10 flex">
+          <div className="relative border border-gray-400 rounded-lg bg-white bg-opacity-10 mt-10 flex">
             <div>
               <input
                 id="name"
                 name="name"
                 type={password1.show ? "text" : "password"}
-                class="w-full  text-white placeholder-transparent  bg-white bg-opacity-0  peer focus:outline-none "
+                className="w-full  text-white placeholder-transparent  bg-white bg-opacity-0  peer focus:outline-none "
                 placeholder="Name"
                 value={password2.value}
                 onChange={(e) => {
@@ -157,8 +157,8 @@ export default function Register() {
                 autoComplete="off"
               />
               <label
-                for="name"
-                class="absolute left-0 pl-2 -top-7 text-white font-bold text-md transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-white peer-placeholder-shown:top-2 peer-focus:-top-7 peer-focus:text-white peer-focus:text-md"
+                form="name"
+                className="absolute left-0 pl-2 -top-7 text-white font-bold text-md transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-white peer-placeholder-shown:top-2 peer-focus:-top-7 peer-focus:text-white peer-focus:text-md"
               >
                 Confirm Password:
               </label>
@@ -177,18 +177,18 @@ export default function Register() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="w-6 h-6"
+                  className="w-6 h-6"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
                   />
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
                   />
                 </svg>
@@ -197,13 +197,13 @@ export default function Register() {
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
-                  stroke-width="1.5"
+                  strokeWidth="1.5"
                   stroke="currentColor"
-                  class="w-6 h-6"
+                  className="w-6 h-6"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
                   />
                 </svg>
--- components/UsernameForm.js
+++ components/UsernameForm.js
@@ -21,25 +21,25 @@ export default function UsernameForm() {
   //   return () => clearTimeout(delayDebounceFn);
   // }, [userName]);
 
-  const checkUsername = useCallback(
-    debounce(async function () {
-      const valid = await userNameAvailable(userName);
+  const checkUsername = useCallback( 
+    debounce(async function (name) {
+      const valid = await userNameAvailable(name);
       setValid(valid);
-      console.log(valid)
+      console.log(name, valid)
     }, 500),
     []
   );
 
   const handleChange = (name) => {
     setuserName(name);
+    //TODO set valid to 'pending' or false or something before checking
     checkUsername(name);
   };
 
   return (
-    <div class="main" className=" flex justify-center">
+    <div  className=" flex justify-center">
       {/* card */}
       <div
-        class="card"
         className=" bg-gray-300 backdrop-blur-sm bg-opacity-20 flex flex-col items-center justify-center p-4 shadow-lg rounded-2xl xl:w-1/2"
       >
         <div className="p-5">
@@ -49,19 +49,19 @@ export default function UsernameForm() {
           </h1>
         </div>
         <div className="relative w-full mb-3 pt-5">
-          <div class="relative border border-gray-400 rounded-lg">
+          <div className="relative border border-gray-400 rounded-lg">
             <input
               id="name"
               name="name"
               type="text"
-              class="w-full  text-white placeholder-transparent  bg-white bg-opacity-10  peer focus:outline-none "
+              className="w-full  text-white placeholder-transparent  bg-white bg-opacity-10  peer focus:outline-none "
               placeholder="Name"
               value={userName}
               onChange={(e) => handleChange(e.target.value)}
               autoComplete="off"
             />
             <label
-              for="name"
+              form="name"
               className="absolute left-0 pl-2 -top-7 text-white font-bold text-md transition-all peer-placeholder-shown:text-base peer-placeholder-shown:text-white peer-placeholder-shown:top-2 peer-focus:-top-7 peer-focus:text-white peer-focus:text-md"
             >
               Username:
@@ -74,13 +74,13 @@ export default function UsernameForm() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="#32CD32"
-              class="w-10 h-10"
+              className="w-10 h-10"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
@@ -89,13 +89,13 @@ export default function UsernameForm() {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="#ff0000"
-              class="w-10 h-10"
+              className="w-10 h-10"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
